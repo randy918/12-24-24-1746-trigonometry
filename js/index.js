e7f88758f3d60151ba1981c2131f6f4e5bc8c59c
Randy Miller <randy918@gmail.com>
2024-12-24 18:10:01 -0600

    good initial trignometry  12-24-24-1809

---
+++ js/index.js
@@ -0,0 +1,58 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here
+
+    document.body.style.backgroundColor = 'green';
+
+    //>  ████████████████████████████████████  CHILDREN
+
+
+
+
+
+
+    //>  ███████████████ OPEN WEATHER API d1a7fcdb2d6f38c6b216b37322444de9
+
+    //>  ████████████████████████████████████  BRIGHT BACKGROUND
+
+    function getBrightRandomRGB() {
+        const rrr = r(86, 255);
+        const ggg = r(86, 255);
+        const bbb = r(86, 255);
+        const rgbColor = `rgb(${rrr}, ${ggg}, ${bbb})`;
+        return rgbColor;
+    }
+
+    document.body.style.backgroundColor = getBrightRandomRGB();
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+
+    //>  ████████████████████████████████████  GENERAL PROGRAM
+
+   
+
+
+
+
+
+
+
+
+    //>  ████████████████████████████████████  GENERAL PROGRAM
+
+
+})
+
+
+
+
+
+// console.log(document.documentElement);
+
+//< ████████████████████████████████████  MISC
+
+
+
