08887359dc1f97d39923ba96dd4214b7a92d7f4f
Randy Miller <randy918@gmail.com>
2024-12-25 12:01:50 -0600

    just abc triangle sides now 12-25-24-1201

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Your code here
-
-    document.body.style.backgroundColor = 'green';
-
-    //>  ████████████████████████████████████  CHILDREN
 
 
-
-
-
-
-    //>  ███████████████ OPEN WEATHER API d1a7fcdb2d6f38c6b216b37322444de9
 
     //>  ████████████████████████████████████  BRIGHT BACKGROUND
 
@@ -22,28 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const rgbColor = `rgb(${rrr}, ${ggg}, ${bbb})`;
         return rgbColor;
     }
-
     document.body.style.backgroundColor = getBrightRandomRGB();
-
-    //_ ████████████████████████████████████  DATA STRUCTURES 
-
-    //_ ████████████████████████████████████  DATA STRUCTURES 
-
 
     //>  ████████████████████████████████████  INITIAL VARIABLES
 
     const myCanvas = document.getElementById('myCanvas');
     const ctx = myCanvas.getContext("2d");
 
-    const tallFont = new FontFace('bold', 'url("../fonts/FiraSans-Black.ttf")');
-    tallFont.load().then(function (loadedFont) {
-        document.fonts.add(loadedFont);
-    });
-    function resizeCanvas() {
-        const canvasWidth = myCanvas.offsetWidth; // Get pixel width
-        myCanvas.width = canvasWidth;
-        myCanvas.height = canvasWidth * 1; // Maintain aspect ratio (50/80) 
-    }
+
 
     resizeCanvas(); // Initial setup
 
@@ -57,14 +33,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const B = { x: 90, y: 120 }
     const C = { x: B.x, y: 0 }
 
+    //^ ████████████████████████████████████  EVENTS    
+
+    update();
+
+    document.onmousemove = (event) => {
+        B.x = event.x - offset.x;
+        B.y = event.y - offset.y;
+        C.x = B.x;
+        update();
+    }
+
     //_ ████████████████████████████████████  DRAW FUNCTIONS 
+
+    function resizeCanvas() {
+        const canvasWidth = myCanvas.offsetWidth; // Get pixel width
+        myCanvas.width = canvasWidth;
+        myCanvas.height = canvasWidth * 1; // Maintain aspect ratio (50/80) 
+    }
 
     function drawPoint(location, size = 60, color = "red") {
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(location.x, location.y, size / 2, 0, Math.PI * 2);
         ctx.fill();
+
     }
+
+
+    function drawLine(p1, p2, color = 'black', width = 8) {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.stroke();
+    }
+
 
     function drawCoordinateSystem(ctx, offset) {
         ctx.beginPath();
@@ -78,26 +83,52 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function drawText(text, location, color = "white") {
+        ctx.font = "bold 60px ralewayLight";
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
-        ctx.font = "bold 50px tall";
         ctx.fillText(text, location.x, location.y);
     }
 
-    //>  ████████████████████████████████████  GENERAL PROGRAM
+    function average(p1, p2) {
+        return {
+            x: (p1.x + p2.x) / 2,
+            y: (p1.y + p2.y) / 2
+        };
+    }
+    
+    function distance(p1, p2) {
+        return Math.hypot(p1.x-p2.x,p1.y-p2.y);
+    }
+    
 
-    drawCoordinateSystem(ctx, offset);
 
-    drawPoint(A);
-    drawText("A", A);
-    drawPoint(B);
-    drawText("B", B);
-    drawPoint(C);
-    drawText("C", C);
+    //>  ████████████████████████████████████  MAIN
+
+    function update() {
+
+        const c = distance(A, B);
+        const a = distance(B, C);
+        const b = distance(A, C);
+
+
+        ctx.clearRect(-offset.x, -offset.y, myCanvas.width, myCanvas.height);
+
+        drawCoordinateSystem(ctx, offset);
+
+        drawLine(A, B, "red", 8);
+        drawText('c',average(A, B), "white");
+        drawLine(A, C, "orange", 8);
+        drawText('b',average(A, C), "white");
+        drawLine(B, C, "yellow", 8);
+        drawText('a',average(B, C), "white");
+
+
+    }
 })
 
+//_  ████████████████████████████████████  GENERAL PROGRAM
 
 
 
